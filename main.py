b81f1917108420a5281310d1533325381d331de7
william Chero <cherowilliam12@gmail.com>
2026-06-29 12:15:02 -0500

    correccion /

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://skillchero.github.io/sistema-vocacional-ie/"
+        "https://skillchero.github.io/sistema-vocacional-ie"
     ], 
     allow_credentials=True,
     allow_methods=["*"],
