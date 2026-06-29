6351c7d07050213a9ee8c68da05ff75c324e5e4c
william Chero <cherowilliam12@gmail.com>
2026-06-28 23:13:57 -0500

    Permitir CORS desde GitHub Pages

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://skillchero.github.io/sistema-vocacional-ie/"
     ], 
     allow_credentials=True,
     allow_methods=["*"],
